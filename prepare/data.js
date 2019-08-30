fe4f056d6a4a54f47668d181a3c739c336f271e2
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2019-08-30 09:19:47 +0200

    read feed parameter

--- prepare/data.js
+++ prepare/data.js
@@ -1,7 +1,8 @@
 const gdx = require('node-gdx')
+const _ = require('lodash')
 
-const file = process.argv[2]
-
+// const file = process.argv[2]
+const file = 'C:/Users/pahmeyer/Documents/FarmDyn/results/SustainBeef/Fr.Cant-CC.gdx'
 ;(async () => {
   const data = await gdx.read(file)
   const cropHa = data.p_crop.filter(d => d['1'] === 'cropHA').map(d => {
@@ -16,8 +17,8 @@ const file = process.argv[2]
   const soldOutputQuant = data.p_other.filter(d => d['1'] === 'SoldOutputQuant').map(d => {
     return [d['2'],d.Value]
   })
-  const feedByHerdMonth = data.p_other.filter(d => d['3'] === 'Feed').map(d => {
-    return [d['2'],d.Value]
-  })
-  console.log(soldOutputQuant);
+  const feedHerdsByMonth = _.groupBy(data.p_other.filter(d => d['3'] === 'Feed').map(d => {
+    return [d['1'],d['2'],d['4'],d.Value]
+  }), a => a[1])
+  console.log(feedHerdsByMonth);
 })()
--- run.cmd
+++ run.cmd
@@ -1,2 +1,1 @@
-N:\soft\GAMS28.2\GMSPython\python -m http.server 1337
-start "http://localhost:1337/"
+N:\soft\GAMS28.2\GMSPython\python -m http.server 1337 && start "http://localhost:1337/"
